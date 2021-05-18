097c7fd4ec0b97a611890e6cb141e8cf0da919f5
kaveeshprashan <kaveeshprasan@gmail.com>
2021-05-18 23:11:59 +0530

    added the files

---
+++ standAlone/src/sample/Controller.java
@@ -0,0 +1,346 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+
+import java.util.ResourceBundle;
+
+import static java.lang.Integer.parseInt;
+
+public class Controller implements Initializable {
+    @FXML
+    private Label movie;
+    @FXML
+    private Label time;
+    @FXML
+    private Label tickets;
+    @FXML
+    private Label total;
+    @FXML
+    private Label seats;
+    @FXML
+    private Button b1;
+    @FXML
+    private Button b2;
+    @FXML
+    private Button b3;
+    @FXML
+    private Button b4;
+    @FXML
+    private Button b5;
+    @FXML
+    private Button b6;
+    @FXML
+    private Button b7;
+    @FXML
+    private Button b8;
+    @FXML
+    private Button b9;
+    @FXML
+    private Button b10;
+    @FXML
+    private Button b11;
+    @FXML
+    private Button b12;
+    @FXML
+    private Button b13;
+    @FXML
+    private Button b14;
+    @FXML
+    private Button b15;
+    @FXML
+    private Button b16;
+    @FXML
+    private Button b17;
+    @FXML
+    private Button b18;
+    @FXML
+    private Button b19;
+    @FXML
+    private Button b20;
+    @FXML
+    private Button b21;
+    @FXML
+    private Button b22;
+    @FXML
+    private Button b23;
+    @FXML
+    private Button b24;
+    @FXML
+    private Button b25;
+    @FXML
+    private Button b26;
+    @FXML
+    private Button b27;
+    @FXML
+    private Button b28;
+    @FXML
+    private Button b29;
+    @FXML
+    private Button b30;
+    @FXML
+    private Button b31;
+    @FXML
+    private Button b32;
+    @FXML
+    private Button b33;
+    @FXML
+    private Button b34;
+    @FXML
+    private Button b35;
+    @FXML
+    private Button b36;
+    @FXML
+    private Button b37;
+    @FXML
+    private Button b38;
+    @FXML
+    private Button b39;
+    @FXML
+    private Button b40;
+    @FXML
+    private Button b41;
+    @FXML
+    private Button b42;
+    @FXML
+    private Button b43;
+    @FXML
+    private Button b44;
+    @FXML
+    private Button b45;
+    @FXML
+    private Button b46;
+    @FXML
+    private Button b47;
+    @FXML
+    private Button b48;
+    @FXML
+    private Button b49;
+    @FXML
+    private Button b50;
+    @FXML
+    private Button b51;
+    @FXML
+    private Button b52;
+    @FXML
+    private Button b53;
+    @FXML
+    private Button b54;
+    @FXML
+    private Button b55;
+    @FXML
+    private Button b56;
+    @FXML
+    private Button b57;
+    @FXML
+    private Button b58;
+    @FXML
+    private Button b59;
+    @FXML
+    private Button b60;
+    @FXML
+    private Button b61;
+    @FXML
+    private Button b62;
+    @FXML
+    private Button b63;
+    @FXML
+    private Button b64;
+    @FXML
+    private Button b65;
+    @FXML
+    private Button b66;
+    @FXML
+    private Button b67;
+    @FXML
+    private Button b68;
+    @FXML
+    private Button b69;
+    @FXML
+    private Button b70;
+    @FXML
+    private Button b71;
+    @FXML
+    private Button b72;
+    @FXML
+    private Button b73;
+    @FXML
+    private Button b74;
+    @FXML
+    private Button b75;
+    @FXML
+    private Button b76;
+    @FXML
+    private Button b77;
+    @FXML
+    private Button b78;
+    @FXML
+    private Button b79;
+    @FXML
+    private Button b80;
+    @FXML
+    private Button b81;
+    @FXML
+    private Button b82;
+    @FXML
+    private Button b83;
+    @FXML
+    private Button b84;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        movie.setText("Gingerclown");
+        time.setText("April 3,21.00");
+        b1.setStyle("-fx-background-color:slateGrey");
+        b2.setStyle("-fx-background-color:slateGrey");
+        b3.setStyle("-fx-background-color:slateGrey");
+        b4.setStyle("-fx-background-color:slateGrey");
+        b5.setStyle("-fx-background-color:slateGrey");
+        b6.setStyle("-fx-background-color:slateGrey");
+        b7.setStyle("-fx-background-color:slateGrey");
+        b8.setStyle("-fx-background-color:slateGrey");
+        b9.setStyle("-fx-background-color:slateGrey");
+        b10.setStyle("-fx-background-color:slateGrey");
+        b11.setStyle("-fx-background-color:slateGrey");
+        b12.setStyle("-fx-background-color:slateGrey");
+        b13.setStyle("-fx-background-color:slateGrey");
+        b14.setStyle("-fx-background-color:slateGrey");
+        b15.setStyle("-fx-background-color:slateGrey");
+        b16.setStyle("-fx-background-color:slateGrey");
+        b17.setStyle("-fx-background-color:slateGrey");
+        b18.setStyle("-fx-background-color:slateGrey");
+        b19.setStyle("-fx-background-color:slateGrey");
+        b20.setStyle("-fx-background-color:slateGrey");
+        b21.setStyle("-fx-background-color:slateGrey");
+        b22.setStyle("-fx-background-color:slateGrey");
+        b23.setStyle("-fx-background-color:slateGrey");
+        b24.setStyle("-fx-background-color:slateGrey");
+        b25.setStyle("-fx-background-color:slateGrey");
+        b26.setStyle("-fx-background-color:slateGrey");
+        b27.setStyle("-fx-background-color:slateGrey");
+        b28.setStyle("-fx-background-color:slateGrey");
+        b29.setStyle("-fx-background-color:slateGrey");
+        b30.setStyle("-fx-background-color:slateGrey");
+        b31.setStyle("-fx-background-color:slateGrey");
+        b32.setStyle("-fx-background-color:slateGrey");
+        b33.setStyle("-fx-background-color:slateGrey");
+        b34.setStyle("-fx-background-color:slateGrey");
+        b35.setStyle("-fx-background-color:slateGrey");
+        b36.setStyle("-fx-background-color:slateGrey");
+        b37.setStyle("-fx-background-color:slateGrey");
+        b38.setStyle("-fx-background-color:slateGrey");
+        b39.setStyle("-fx-background-color:slateGrey");
+        b40.setStyle("-fx-background-color:slateGrey");
+        b41.setStyle("-fx-background-color:slateGrey");
+        b42.setStyle("-fx-background-color:slateGrey");
+        b43.setStyle("-fx-background-color:slateGrey");
+        b44.setStyle("-fx-background-color:slateGrey");
+        b45.setStyle("-fx-background-color:slateGrey");
+        b46.setStyle("-fx-background-color:slateGrey");
+        b47.setStyle("-fx-background-color:slateGrey");
+        b48.setStyle("-fx-background-color:slateGrey");
+        b49.setStyle("-fx-background-color:slateGrey");
+        b50.setStyle("-fx-background-color:slateGrey");
+        b51.setStyle("-fx-background-color:slateGrey");
+        b52.setStyle("-fx-background-color:slateGrey");
+        b53.setStyle("-fx-background-color:slateGrey");
+        b54.setStyle("-fx-background-color:slateGrey");
+        b55.setStyle("-fx-background-color:slateGrey");
+        b56.setStyle("-fx-background-color:slateGrey");
+        b57.setStyle("-fx-background-color:slateGrey");
+        b58.setStyle("-fx-background-color:slateGrey");
+        b59.setStyle("-fx-background-color:slateGrey");
+        b60.setStyle("-fx-background-color:slateGrey");
+        b61.setStyle("-fx-background-color:slateGrey");
+        b62.setStyle("-fx-background-color:slateGrey");
+        b63.setStyle("-fx-background-color:slateGrey");
+        b64.setStyle("-fx-background-color:slateGrey");
+        b65.setStyle("-fx-background-color:slateGrey");
+        b66.setStyle("-fx-background-color:slateGrey");
+        b67.setStyle("-fx-background-color:slateGrey");
+        b68.setStyle("-fx-background-color:slateGrey");
+        b69.setStyle("-fx-background-color:slateGrey");
+        b70.setStyle("-fx-background-color:slateGrey");
+        b71.setStyle("-fx-background-color:slateGrey");
+        b72.setStyle("-fx-background-color:slateGrey");
+        b73.setStyle("-fx-background-color:slateGrey");
+        b74.setStyle("-fx-background-color:slateGrey");
+        b75.setStyle("-fx-background-color:slateGrey");
+        b76.setStyle("-fx-background-color:slateGrey");
+        b77.setStyle("-fx-background-color:slateGrey");
+        b78.setStyle("-fx-background-color:slateGrey");
+        b79.setStyle("-fx-background-color:slateGrey");
+        b80.setStyle("-fx-background-color:slateGrey");
+        b81.setStyle("-fx-background-color:slateGrey");
+        b82.setStyle("-fx-background-color:slateGrey");
+        b83.setStyle("-fx-background-color:slateGrey");
+        b84.setStyle("-fx-background-color:slateGrey");
+
+    }
+
+
+    ObservableList<Button> list = FXCollections.observableArrayList();
+
+
+    public void ButtonClicked(ActionEvent event) {
+
+
+        if (((Button) event.getTarget()).getStyle() == "-fx-background-color:slateGrey") {
+            ((Button) event.getTarget()).setStyle("-fx-background-color:lime");
+
+            if (parseInt(((Button) event.getTarget()).getId()) > 78) {
+                total.setText(String.valueOf(parseInt(total.getText()) + 20));
+                seats.setText(String.valueOf(parseInt(seats.getText()) - 1));
+                tickets.setText(String.valueOf(parseInt(tickets.getText()) + 1));
+            } else {
+                total.setText(String.valueOf(parseInt(total.getText()) + 12));
+                seats.setText(String.valueOf(parseInt(seats.getText()) - 1));
+                tickets.setText(String.valueOf(parseInt(tickets.getText()) + 1));
+            }
+
+            list.add(((Button) event.getSource()));
+
+        } else if (((Button) event.getTarget()).getStyle() == "-fx-background-color:lime") {
+            ((Button) event.getTarget()).setStyle("-fx-background-color:slateGrey");
+            if (parseInt(((Button) event.getTarget()).getId()) > 78) {
+                total.setText(String.valueOf(parseInt(total.getText()) - 20));
+                seats.setText(String.valueOf(parseInt(seats.getText()) + 1));
+                tickets.setText(String.valueOf(parseInt(tickets.getText()) - 1));
+            } else {
+                total.setText(String.valueOf(parseInt(total.getText()) - 12));
+                seats.setText(String.valueOf(parseInt(seats.getText()) - 1));
+                tickets.setText(String.valueOf(parseInt(tickets.getText()) - 1));
+            }
+
+            list.remove(((Button) event.getTarget()));
+
+        }
+
+    }
+
+    public void BookNowButtonClicked(ActionEvent event) {
+
+
+        for (Button button : list) {
+
+            button.setDisable(true);
+            button.setStyle("-fx-disabled-opacity: 1.0");
+            button.setStyle("-fx-background-color:red");
+
+
+        }
+
+        total.setText("0");
+        tickets.setText("0");
+
+    }
+}
